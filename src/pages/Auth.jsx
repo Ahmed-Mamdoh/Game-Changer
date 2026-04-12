b3a6bc0b85ed3188b74dc996a695dfe68d9d4c05
Ahmed Mamdoh <akremmamdoh@gmail.com>
2026-04-12 16:32:40 +0200

    fix: enhance layout in Auth component for improved responsiveness

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -2,10 +2,10 @@ import { AuthForm } from "./../features/Auth/AuthForm";
 import FadingBackground from "./../features/games/ui/FadingBackground";
 function Auth() {
   return (
-    <div className="relative mx-auto">
+    <div className="relative mx-auto min-h-screen py-20">
       <FadingBackground first={true} />
       {/* Content section */}
-      <div className="absolute top-[10vh] z-10 w-full">
+      <div className=" z-10 w-full">
         <AuthForm />
       </div>
     </div>
