b555d7980a1d4f02c7aa9342825556f1158db071
Ahmed Mamdoh <akremmamdoh@gmail.com>
2026-04-15 15:58:22 +0200

    refactor(games): extract filter params logic into separate hook

--- src/features/games/hooks/useGetAllGames.js
+++ src/features/games/hooks/useGetAllGames.js
@@ -1,38 +1,17 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getAllGames } from "../../../api/igdbApi";
-import { useSearchParams } from "react-router-dom";
+import useGetFiltersParams from "./useGetFiltersParams";
+
 export function useGetAllGames() {
+  const { filters, sortBy, page, search } = useGetFiltersParams();
   const queryClient = useQueryClient();
 
-  const [searchParams] = useSearchParams();
-  const search = searchParams.get("search");
-  const page = Number(searchParams.get("page")) || 1;
-  const genre = searchParams.get("genre");
-  const themes = searchParams.get("theme");
-  const gameMode = searchParams.get("gameMode");
-  const sortBy = search
-    ? null
-    : searchParams.get("sortBy") || "total_rating_count";
-  let filters = [];
-  if (genre) {
-    filters.push(`genres = [${genre.split("-")}]`);
-  }
-  if (themes) {
-    filters.push(`themes = [${themes.split("-")}]`);
-  }
-  if (gameMode) {
-    filters.push(`game_modes = (${gameMode.split("-")})`);
-  }
-  console.log(filters);
-  console.log(sortBy);
-  console.log(page);
-  console.log(search);
   const { data, isLoading, error } = useQuery({
     queryKey: ["games", "all", page, search, filters, sortBy],
     queryFn: () =>
       getAllGames({
         filters,
-        sortBy,
+        sortBy: sortBy || "total_rating_count",
         page,
         search,
       }),
@@ -43,7 +22,7 @@ export function useGetAllGames() {
     queryFn: () =>
       getAllGames({
         filters,
-        sortBy,
+        sortBy: sortBy || "total_rating_count",
         page: page + 1,
         search,
       }),
