e064baff3f35fc8862d4d1fcb501b218fe8eae2f
Ahmed Mamdoh <akremmamdoh@gmail.com>
2026-04-25 14:54:01 +0300

    chore: switch to automated sitemap generation

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import sitemapPlugin from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemapPlugin({ hostname: "https://game-changer-gg.vercel.app" }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
